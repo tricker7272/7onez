77250ee8e274deafaffa6145e38e03a77164a36a
Saksham Shekher <67940483+Oshekher@users.noreply.github.com>
2021-05-24 07:54:46 +0530

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,15 +1,15 @@
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-echo Mencari Admin Akses!
+echo Looking for Admin Access!
 goto UACPrompt
-) else ( goto KetemuAdmin )
+) else ( goto Meet Admin )
 :UACPrompt
 echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
 echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 "%temp%\getadmin.vbs"
 exit /B
-:KetemuAdmin
+:Meet Admin
 if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
 pushd "%CD%"
 CD /D "%~dp0"
