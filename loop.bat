8eef7f6fdd7ce3064adaba7ee39216390f1cbe6a
ngork <85639222+ngork@users.noreply.github.com>
2021-06-10 00:40:28 +0530

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,8 +1,6 @@
 @echo off
-echo AdityaRDP By Oshekher!
 tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, be sure NGROK_AUTH_TOKEN right in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
-echo AdityaRDP By Oshekher!
 goto check
